7efc19a126539e01c35e0e8577095b00b3d40057
André Angeloni <andrefangeloni@gmail.com>
2022-07-18 21:26:40 -0300

    feat: setup svg

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,8 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { VStack, Heading } from 'native-base';
 
-export const SignIn = () => (
-  <View>
-    <Text>SignIn</Text>
-  </View>
-);
+import Logo from '../assets/logo_primary.svg';
+
+export const SignIn = () => {
+  return (
+    <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
+      <Logo />
+
+      <Heading color="gray.100" fontSize="xl" mt={20} mb={6}>
+        Acesse sua conta
+      </Heading>
+    </VStack>
+  );
+};
