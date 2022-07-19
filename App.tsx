a706e15846a5ea253c1b6c78ee40523d61060a96
André Angeloni <andrefangeloni@gmail.com>
2022-07-18 21:16:48 -0300

    feat: setup statusbar

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
 import React from 'react';
-import { NativeBaseProvider } from 'native-base';
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import {
+  useFonts,
+  Roboto_700Bold,
+  Roboto_400Regular,
+} from '@expo-google-fonts/roboto';
+
+import { Loading } from './src/components/Loading';
 
 import { SignIn } from './src/screens/SignIn';
 
 import { THEME } from './src/styles/theme';
 
-export const App = () => (
-  <NativeBaseProvider theme={THEME}>
-    <SignIn />
-  </NativeBaseProvider>
-);
+export const App = () => {
+  const [fontsLoaded] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular,
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        translucent
+        barStyle="light-content"
+        backgroundColor="transparent"
+      />
+
+      {fontsLoaded ? <SignIn /> : <Loading />}
+    </NativeBaseProvider>
+  );
+};
