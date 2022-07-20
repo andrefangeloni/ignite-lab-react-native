dc4b05b60e2ef18df9c19f51ce8b692d4e020753
André Angeloni <andrefangeloni@gmail.com>
2022-07-19 23:04:37 -0300

    feat: firebase auth integrated

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,33 @@
+import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
+
+import { Loading } from '../components/Loading';
+
+import { SignIn } from '../screens/SignIn';
 
 import { AppRoutes } from './app.routes';
 
-export const Routes = () => (
-  <NavigationContainer>
-    <AppRoutes />
-  </NavigationContainer>
-);
+export const Routes = () => {
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [user, setUser] = React.useState<FirebaseAuthTypes.User>();
+
+  React.useEffect(() => {
+    const subscriber = auth().onAuthStateChanged((response) => {
+      setUser(response);
+      setIsLoading(false);
+    });
+
+    return subscriber;
+  }, []);
+
+  if (isLoading) {
+    return <Loading />
+  }
+
+  return (
+    <NavigationContainer>
+      {user ? <AppRoutes /> : <SignIn />}
+    </NavigationContainer>
+  );
+};
--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Alert } from 'react-native';
+import auth from '@react-native-firebase/auth';
 import { Envelope, Key } from 'phosphor-react-native';
 import { VStack, Heading, Icon, useTheme } from 'native-base';
 
@@ -12,6 +14,31 @@ export const SignIn = () => {
 
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  const [isLoading, setIsLoading] = React.useState(false);
+
+  const handleSignIn = async () => {
+    if (!email || !password) {
+      return Alert.alert('Atenção', 'Informe e-mail e senha');
+    }
+
+    try {
+      setIsLoading(true);
+
+      await auth().signInWithEmailAndPassword(email, password);
+    } catch (e: any) {
+      switch (e.code) {
+        case 'auth/invalid-email':
+          return Alert.alert('Atenção', 'E-mail inválido');
+        case 'auth/wrong-password':
+        case 'auth/user-not-found':
+          return Alert.alert('Atenção', 'E-mail ou senha inválida');
+        default:
+          return Alert.alert('Erro inesperado', 'Tente novamente mais tarde');
+      }
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
   return (
     <VStack flex={1} alignItems="center" bg="gray.600" px={8} pt={24}>
@@ -39,7 +66,12 @@ export const SignIn = () => {
         InputLeftElement={<Icon as={<Key color={colors.gray[300]} />} ml={4} />}
       />
 
-      <Button title="Entrar" w="full" />
+      <Button
+        w="full"
+        title="Entrar"
+        isLoading={isLoading}
+        onPress={() => handleSignIn()}
+      />
     </VStack>
   );
 };
